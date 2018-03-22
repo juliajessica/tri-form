e6216f135a2c0e9574a640e66c18611a3d9f09d9
cory west-forbes and julia sheremet <julia@>
2018-03-22 16:47:14 -0700

    refactor conditional

--- js/scripts.js
+++ js/scripts.js
@@ -9,12 +9,12 @@ $(document).ready(function(){
     function triangle(){
       if ((side1 + side2 < side3) || (side2 + side3 < side1) || (side1 + side3 < side2)) {
         $(".user-output").text("What are you? Because you are definitely not a triangle!");
+      } else if (side1 === side2 && side1 === side3 && side2 === side3) {
+        $(".user-output").text("You are an Equilateral!");
       } else if ((side1 === side2 && side1 !== side3) || (side2 === side3 && side2 !== side1) || (side3 === side1 && side3 !== side1)) {
         $(".user-output").text("You are an Isosceles!");
       } else if (side1 !== side2 !== side3) {
         $(".user-output").text("You are a Scalene!");
-      } else if (side1 === side2 && side1 === side3 && side2 === side3) {
-        $(".user-output").text("You are an Equilateral!");
       }
     }
     triangle();
